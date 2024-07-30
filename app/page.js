8b8fd7dd31872d29ea326cc48e1ddb2d5a62a679
NotJerwee <jerrylin2488@gmail.com>
2024-07-29 23:23:47 -0400

    Add outside box and border to box

--- app/page.js
+++ app/page.js
@@ -19,40 +19,41 @@ export default function Home() {
   justifyContent={"center"}
   flexDirection={"column"}
   alignItems={"center"}>
-    <Box 
-      width="800px" 
-      height="100px"
-      bgcolor={'#ADD8E6'} 
-      display={"flex"}
-      justifyContent={"center"}
-      alignItems={"center"} 
-      border={"1px solid #333"}
-      >
-      <Typography variant={"h2"} color={"#333"} textAlign={"center"}>
-        Pantry Items
-      </Typography>
+    <Box border={"1px solid #333"}>
+      <Box 
+        width="800px" 
+        height="100px"
+        bgcolor={'#ADD8E6'} 
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"} 
+        >
+        <Typography variant={"h2"} color={"#333"} textAlign={"center"}>
+          Pantry Items
+        </Typography>
+      </Box>
+      <Stack width="800px" height="300px" spacing={2} overflow={"auto"}>
+        {items.map((i) => (
+          <Box
+            key={i}
+            width="100%"
+            height="300px"
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            bgcolor={'#f0f0f0'}>
+              <Typography
+                variant={"h3"}
+                color={"#333"}
+                textAlign={"center"}>
+                {
+                  i.charAt(0).toUpperCase() + i.slice(1) 
+                }
+              </Typography>
+          </Box>
+        ))}
+      </Stack>
     </Box>
-    <Stack width="800px" height="300px" spacing={2} overflow={"auto"}>
-      {items.map((i) => (
-        <Box
-          key={i}
-          width="100%"
-          height="300px"
-          display={"flex"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          bgcolor={'#f0f0f0'}>
-            <Typography
-              variant={"h3"}
-              color={"#333"}
-              textAlign={"center"}>
-              {
-                i.charAt(0).toUpperCase() + i.slice(1) 
-              }
-            </Typography>
-        </Box>
-      ))}
-    </Stack>
   </Box>
   
 }
