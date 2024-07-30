97982a19b64371a44a4cdb606f6eca11bdf5c320
NotJerwee <jerrylin2488@gmail.com>
2024-07-29 22:55:40 -0400

    Add custom css to items

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Stack, Box, Typography } from '@mui/material'
 
 const items = [
   "tomato",
@@ -6,6 +6,9 @@ const items = [
   "onion",
   "garlic",
   "ginger",
+  "carrot",
+  "apple",
+  "banana"
 ]
 
 export default function Home() {
@@ -15,7 +18,26 @@ export default function Home() {
   display={"flex"}
   justifyContent={"center"}
   alignItems={"center"}>
-    
+    <Stack width="800px" height="300px" spacing={2} overflow={"auto"}>
+      {items.map((i) => (
+        <Box
+          key={i}
+          width="100%"
+          height="300px"
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          bgcolor={'#f0f0f0'}>
+            <Typography
+              variant={"h4"}
+              color={"#333"}
+              textAlign={"center"}
+              fontWeight={"bold"}>
+              {i}
+            </Typography>
+        </Box>
+      ))}
+    </Stack>
   </Box>
   
 }
