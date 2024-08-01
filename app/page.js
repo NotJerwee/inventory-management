1fef252e0f755139fb1729a7c8dda618f52e4c65
NotJerwee <jerrylin2488@gmail.com>
2024-08-01 15:37:29 -0400

    Implemennt firebase fetching

--- app/page.js
+++ app/page.js
@@ -21,6 +21,20 @@ export default function Home() {
   const [open, setOpen] = useState(false)
   const [itemName, setItemName] = useState('')
 
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc) => {
+      inventoryList.push({ name: doc.id, ...doc.data() })
+    })
+    setInventory(inventoryList)
+  }
+  
+  useEffect(() => {
+    updateInventory()
+  }, [])
+  
   return <Box
   width="100ww"
   height="100vh"
