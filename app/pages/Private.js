73eeceaf238eb493c0d440a30b83faee5b6efad3
NotJerwee <jerrylin2488@gmail.com>
2024-08-14 23:29:17 -0400

    Update Private.js

---
+++ app/pages/Private.js
@@ -0,0 +1,17 @@
+// Firebase auth
+import { signOut } from "firebase/auth"
+import { auth } from "../firebase"
+
+export const Private = () => {
+    const handleSignOut = () => {
+        signOut(auth)
+        .then(() => console.log('Sign Out'))
+        .catch((error) => console.log(error))
+    }
+    return (
+        <section>
+            <h2>Private</h2>
+            <button onClick={handleSignOut}>Sign Out</button>
+        </section>
+    )
+}
