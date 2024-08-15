a2ddc4ab7351cc2e1077fea6c9736327caad0ba9
NotJerwee <jerrylin2488@gmail.com>
2024-08-14 23:38:00 -0400

    Add multiple routes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,32 @@
-import App from './pages/App';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-export default function Page() {
-  return <App />;
+import Home from './pages/App'; 
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute'; 
+import Private from './pages/Private'; 
+
+function App() {
+  const user = null;
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup user={user} />} />
+        <Route path="/login" element={<Login user={user} />} />
+        <Route 
+          path="/private" 
+          element={
+            <ProtectedRoute user={user}>
+              <Private />
+            </ProtectedRoute>
+          } 
+        />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
