431786a5250cd693f74cb934b7ca3b7a9107d664
NotJerwee <jerrylin2488@gmail.com>
2024-08-14 23:00:00 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
     storageBucket: "YOUR_PROJECT_ID.appspot.com",
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID"
- };
+};
  
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
