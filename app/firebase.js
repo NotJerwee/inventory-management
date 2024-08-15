90c420ba0605e2abf5dcb5a5122f21e498144fd2
NotJerwee <jerrylin2488@gmail.com>
2024-08-14 23:46:58 -0400

    Add comments

--- app/firebase.js
+++ app/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "YOUR_API_KEY",
     authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
